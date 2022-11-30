ff41a5056de9a1a96573ccb8bdcd710db8966df6
maolele <yicheng.it@qq.com>
2022-11-30 20:55:22 +0800

    edit vue output config for flask

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+/*
+ * @Author: maolele02
+ * @Date: 2022-11-20 22:08:13
+ * @LastEditTime: 2022-11-30 20:50:00
+ * @LastEditors: maolele02
+ * @Description: 
+ * @FilePath: \beidou\vue.config.js
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false // 关闭语法检查
+  lintOnSave:false, // 关闭语法检查
+  publicPath: './',
+  outputDir: './dist',   // build输出目录
+  indexPath: './templates/index.html',   // 打包后index的位置
+  assetsDir: 'static/', // 静态资源目录（js, css, img）
+  lintOnSave: false // 是否开启eslint
 })
